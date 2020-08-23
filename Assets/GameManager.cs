327fff1bd5113f8b80beada4203a3e13c3da4272
raymondkwok7877 <44001511+raymondkwok7877@users.noreply.github.com>
2020-08-23 17:30:47 -0400

    Add files via upload

---
+++ Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool GameIsOver;
+    public GameObject gameOverUI;
+
+    void Start()
+    {
+        GameIsOver = false;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (GameIsOver)
+            return;
+
+        if (PlayerStats.Lives <= 0)
+        {
+            EndGame();
+        }
+	}
+
+    private void EndGame()
+    {
+        GameIsOver = true;
+        gameOverUI.SetActive(true);
+    }
+}
---
+++ Assets/NodeUI.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NodeUI : MonoBehaviour
+{
+    private Node target;
+    public GameObject ui;
+    public Text upgradeCost;
+    public Text sellCost;
+    public Button upgradeButton;
+
+    public void SetTarget(Node _target)
+    {
+        target = _target;
+
+        transform.position = target.GetBuildPosition();
+
+        if (!target.isUpgraded)
+        {
+            upgradeCost.text = "$" + target.turretBlueprint.upgradeCost;
+            upgradeButton.interactable = true;
+        }
+
+        else
+        {
+            upgradeCost.text = "MAX LEVEL";
+            upgradeButton.interactable = false;
+        }
+
+        sellCost.text = "$" + target.turretBlueprint.GetSellAmount();
+
+        ui.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        ui.SetActive(false);
+    }
+
+    public void Upgrade()
+    {
+        target.UpgradeTurrent();
+        BuildManager.instance.DeselectNode();
+    }
+
+    public void Sell()
+    {
+        target.SellTurret();
+        BuildManager.instance.DeselectNode();
+    }
+}
